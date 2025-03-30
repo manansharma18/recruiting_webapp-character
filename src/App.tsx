3a9819b52739eef8515b82a10c3614f1b0d97e8d
Manan Sharma <msharma7@manans-mbp-2.home>
2025-03-29 21:32:55 -0400

    chore :task One And Two

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,58 @@
-import { useState } from 'react';
-import './App.css';
-import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from './consts.js';
-
+import { useState } from "react";
+import "./App.css";
+import { CLASS_LIST } from "./consts.ts";
+import { Attributes, Class } from "./types";
+import AttributeList from "./component/AttributeList.tsx";
+import ClassList from "./component/ClassList.tsx";
 
 function App() {
-  const [num, setNum] = useState<number>(0);
+  const [attributes, setAttributes] = useState<Attributes>({
+    Strength: 10,
+    Dexterity: 10,
+    Constitution: 10,
+    Intelligence: 10,
+    Wisdom: 10,
+    Charisma: 10,
+  });
+  const handleIncreaseAttribute = (attribute: keyof Attributes) => {
+    setAttributes((prev) => ({
+      ...prev,
+      [attribute]: prev[attribute] + 1,
+    }));
+  };
+
+  const handleDecreaseAttribute = (attribute: keyof Attributes) => {
+    setAttributes((prev) => ({
+      ...prev,
+      [attribute]: Math.max(0,prev[attribute] - 1),
+    }));
+  };
+
+  const changeColor = (class_in_list: Class) => {
+    const classRequirements = CLASS_LIST[class_in_list];
+    const output = Object.entries(classRequirements).every(
+      ([attr, value]) => attributes[attr as keyof Attributes] >= value
+    );
+    return output;
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Coding Exercise</h1>
       </header>
-      <section className="App-section">
-        <div>
-          Value:
-          {num}
-          <button>+</button>
-          <button>-</button>
-        </div>
-      </section>
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <section className="App-section">
+          <AttributeList
+            attributes={attributes}
+            handleDecreaseAttribute={handleDecreaseAttribute}
+            handleIncreaseAttribute={handleIncreaseAttribute}
+          />
+        </section>
+        <section className="App-section">
+          <ClassList changeColor ={changeColor}/>
+        </section>
+      </div>
     </div>
   );
 }
